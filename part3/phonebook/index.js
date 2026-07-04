b24eb97cad7bac3a6ef9e16b5346055cb288e01f
Lyna Asma ADJAL <lynaasma.adjal@etu.usthb.dz>
2026-07-05 00:51:39 +0100

    added dist

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-
+app.use(express.static('dist'))
 // custom us eof morgan instead of using the 'tiny ' style 
 
 morgan.token('body', (req) => {
@@ -107,7 +107,7 @@ if (nameExists) {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
